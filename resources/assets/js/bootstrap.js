d67dedeb95dbc234f621dedf77f4e583938b3f3a
Marcos <pgmo150086@upemor.edu.mx>
2019-02-20 01:25:46 -0600

    primeros pasos

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,17 +1,8 @@
 
-window._ = require('lodash');
+import 'bootstrap/dist/css/bootstrap.css'
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
